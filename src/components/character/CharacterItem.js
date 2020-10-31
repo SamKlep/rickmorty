bea4f978921c4e7bbbd33bcdf57f400a948b6410
SamKlep <shamdemann23@gmail.com>
2020-10-30 22:33:58 -0500

    Styling.

--- src/components/character/CharacterItem.js
+++ src/components/character/CharacterItem.js
@@ -7,10 +7,12 @@ const CharacterItem = ({ item }) => {
       <div class='card' style={{ width: '18rem' }}>
         <img class='card-img-top' src={item.image} alt='card' />
         <div class='card-body'>
-          <h5 class='card-title'>{item.name}</h5>
-          <p class='card-text'>{item.species}</p>
+          <h5 class='card-title'>
+            <strong>{item.name}</strong>
+          </h5>
+          <p class='card-text lead'>{item.species}</p>
 
-          <a href='/' class='btn btn-primary'>
+          <a href='/' class='btn bg-primary btn-light'>
             Learn more
           </a>
         </div>
